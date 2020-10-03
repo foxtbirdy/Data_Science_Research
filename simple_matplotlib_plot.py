6c50ced4420b88ed70250c4219fa40258dc49fb8
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2020-10-03 08:16:56 +0600

    Update simple_matplotlib_plot.py

--- simple_matplotlib_plot.py
+++ simple_matplotlib_plot.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 numbers_on_y = [1 , 4 , 9 , 16 , 25]
 numbers_on_x = [1, 2, 3, 4, 5]
-# use the linewidth to control the thickness of the scale
 
+# use the linewidth to control the thickness of the scale
 plt.plot(numbers_on_x , numbers_on_y, linewidth = 1)
 
 # use the title to name your program, font size is controlled by fontsize
