d945ff7528510a3aec13daa63add97c472648e66
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2020-10-03 08:16:23 +0600

    Add files via upload

---
+++ simple_matplotlib_plot.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+numbers_on_y = [1 , 4 , 9 , 16 , 25]
+numbers_on_x = [1, 2, 3, 4, 5]
+# use the linewidth to control the thickness of the scale
+
+plt.plot(numbers_on_x , numbers_on_y, linewidth = 1)
+
+# use the title to name your program, font size is controlled by fontsize
+plt.title("Testing the matplotlib", fontsize = 24)
+
+# use xlabel to name the x-axis of your program
+plt.xlabel("Value", fontsize = 14)
+
+# use ylabel to name the y-axis of your program
+plt.ylabel("Squares of Value" , fontsize = 14)
+
+# controls the numbers that appear on the x-axis and y-axis
+plt.tick_params(axis="both" , labelsize = 14)
+
+
+# Display the results
+plt.show()
